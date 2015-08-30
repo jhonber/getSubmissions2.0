4d302da2c8a9628eca1500c84d1afaea58027450
Jhon Jimenez <jhonb3r@gmail.com>
2015-08-30 13:18:29 -0500

    Getting more information about a submission

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 var handle = process.argv[2];
-var url = 'http://codeforces.com/api/user.status?handle=' + handle + '&from=1&count=1';
 var subIds = [];
+var url = 'http://codeforces.com/api/user.status?handle=' + handle + '&from=1&count=1';
+var ext = {'GNU C++': 'cpp', 'GNU C++11': 'cpp', 'GNU C': 'c' ,'Java': 'java', 'Haskell': 'hs',
+  'Pascal':'p', 'Perl': 'pl', 'PHP': 'php', 'Python': 'py', 'Ruby': 'rb', 'JavaScript': 'js'};
+var comment = {'GNU C++': '//', 'GNU C++11': '//', 'GNU C': '//' ,'Java': '//', 'Haskell': '--',
+  'Pascal': '//', 'Perl': '#', 'PHP': '//', 'Python': '#', 'Ruby': '#', 'JavaScript': '//'};
+
 
 request.get(url, function (err, res, body) {
   if (err) console.log(err);
@@ -12,22 +19,34 @@ request.get(url, function (err, res, body) {
     if (data.status == 'OK') {
       var result = data.result;
       for (var i = 0; i < result.length; ++i) {
-        if (result[i].verdict == 'OK') {
-          subIds.push( {subId: result[i].id, contestId: result[i].contestId});
+        var res = result[i];
+        var contestId = res.contestId;
+        var index = res.problem.index;
+        var lang = res.programmingLanguage;
+        var urlProblemStat = 'http://codeforces.com/contest/' + contestId + '/problem/' + index;
+
+        if (res.verdict == 'OK') {
+          subIds.push( {subId: res.id, contestId: contestId, index: index, lang: lang, urlProblemStat: urlProblemStat} );
         }
       }
 
       for (var i = 0; i < subIds.length; ++i) {
         var subId = subIds[i].subId;
-        var contestId = subIds[i].contestId
-        getSourceCode(subId, contestId, function(err, source) {
+        var contestId = subIds[i].contestId;
+        var problemName = subIds[i].index;
+        var urlProblemStat = subIds[i].urlProblemStat;
+        var lang = subIds[i].lang;
+
+        getSourceCode(subId, contestId, function(err, sourceCode) {
           if (err) console.log(err);
           else {
             getContestName(contestId, function (err, contestName) {
               if (err) console.log(err);
               else {
+                sourceCode = comment[lang] + ' ' + urlProblemStat + '\n' + sourceCode;
                 console.log(contestName);
-                console.log(source);
+                console.log(problemName);
+                console.log(sourceCode);
               }
             });
           }
@@ -65,5 +84,11 @@ function getContestName (contestId, callback) {
       callback(false, name);
     }
     else callback(true);
+  });
+}
+
+function writeFile (path, fileName, ext, sourceCode) {
+  fs.writeFile(path + '/' + fileName + '.' + ext, sourceCode, function (err) {
+    if (err) throw err;
   });
 }
